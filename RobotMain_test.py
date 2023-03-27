dcf737062f82c65a10504d0527a8651f43d846ae
zhenliferris <liz6@ferris.edu>
2023-03-27 14:35:23 -0400

    add more files

--- RobotMain_test.py
+++ RobotMain_test.py
@@ -3,7 +3,6 @@ import KeyPressModule as kp
 
 # define motor by gpio pinout number
 motor = Motor(2,3,4,22,17,27)
-
 kp.init()
 
 def main():
@@ -12,9 +11,9 @@ def main():
     elif kp.getKey('DOWN'):
         motor.move(-0.6,0,0.1)
     elif kp.getKey('LEFT'):
-        motor.move(0.5,0.3,0.1)
+        motor.move(0.3,0.3,0.1)
     elif kp.getKey('RIGHT'):
-        motor.move(0.5,-0.3,0.1)
+        motor.move(0.3,-0.3,0.1)
     else:
         motor.stop(0.1)
 
