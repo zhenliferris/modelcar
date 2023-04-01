f4946d9ebeef8394abab8493695c35657b775d5b
zhenliferris <liz6@ferris.edu>
2023-04-01 18:28:19 -0400

    Mucanum wheel function perameters and change BCM pins for manual control

--- ManuallyControl.py
+++ ManuallyControl.py
@@ -2,21 +2,21 @@ from MotorModule import Motor
 import KeyPressModule as kp
 
 # define motor by gpio pinout number
-motor = Motor(2,3,4,22,17,27)
+motor = Motor(13, 19, 26, 21, 16, 20)
 kp.init()
+
 
 def main():
     if kp.getKey('UP'):
-        motor.move(0.6,0,0.1)
+        motor.move(0.6, 0, 0.1)
     elif kp.getKey('DOWN'):
-        motor.move(-0.6,0,0.1)
+        motor.move(-0.6, 0, 0.1)
     elif kp.getKey('LEFT'):
-        motor.move(0.3,0.3,0.1)
+        motor.move(0.3, 0.3, 0.1)
     elif kp.getKey('RIGHT'):
-        motor.move(0.3,-0.3,0.1)
+        motor.move(0.3, -0.3, 0.1)
     else:
         motor.stop(0.1)
-
 
 
 if __name__ == '__main__':
