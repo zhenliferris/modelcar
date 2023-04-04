660e63968bda61e8dd168d3da21548fc952e4ddc
zhenliferris <liz6@ferris.edu>
2023-04-04 12:05:48 -0400

    testing

--- LaneModule.py
+++ LaneModule.py
@@ -3,33 +3,36 @@ import numpy as np
 import utlis
 
 curveList = []
-avgVal=10
+avgVal = 10
 
-def getLaneCurve(img,display=2):
+
+def getLaneCurve(img, display=2):
 
     imgCopy = img.copy()
     imgResult = img.copy()
-    #### STEP 1
+    # STEP 1
     imgThres = utlis.thresholding(img)
 
-    #### STEP 2
+    # STEP 2
     hT, wT, c = img.shape
     points = utlis.valTrackbars()
-    imgWarp = utlis.warpImg(imgThres,points,wT,hT)
-    imgWarpPoints = utlis.drawPoints(imgCopy,points)
+    imgWarp = utlis.warpImg(imgThres, points, wT, hT)
+    imgWarpPoints = utlis.drawPoints(imgCopy, points)
 
-    #### STEP 3
-    middlePoint,imgHist = utlis.getHistogram(imgWarp,display=True,minPer=0.5,region=4)
-    curveAveragePoint, imgHist = utlis.getHistogram(imgWarp, display=True, minPer=0.9)
+    # STEP 3
+    middlePoint, imgHist = utlis.getHistogram(
+        imgWarp, display=True, minPer=0.5, region=4)
+    curveAveragePoint, imgHist = utlis.getHistogram(
+        imgWarp, display=True, minPer=0.9)
     curveRaw = curveAveragePoint - middlePoint
 
-    #### SETP 4
+    # SETP 4
     curveList.append(curveRaw)
-    if len(curveList)>avgVal:
+    if len(curveList) > avgVal:
         curveList.pop(0)
     curve = int(sum(curveList)/len(curveList))
 
-    #### STEP 5
+    # STEP 5
     if display != 0:
         imgInvWarp = utlis.warpImg(imgWarp, points, wT, hT, inv=True)
         imgInvWarp = cv2.cvtColor(imgInvWarp, cv2.COLOR_GRAY2BGR)
@@ -39,9 +42,12 @@ def getLaneCurve(img,display=2):
         imgLaneColor = cv2.bitwise_and(imgInvWarp, imgLaneColor)
         imgResult = cv2.addWeighted(imgResult, 1, imgLaneColor, 1, 0)
         midY = 450
-        cv2.putText(imgResult, str(curve), (wT // 2 - 80, 85), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 0, 255), 3)
-        cv2.line(imgResult, (wT // 2, midY), (wT // 2 + (curve * 3), midY), (255, 0, 255), 5)
-        cv2.line(imgResult, ((wT // 2 + (curve * 3)), midY - 25), (wT // 2 + (curve * 3), midY + 25), (0, 255, 0), 5)
+        cv2.putText(imgResult, str(curve), (wT // 2 - 80, 85),
+                    cv2.FONT_HERSHEY_COMPLEX, 2, (255, 0, 255), 3)
+        cv2.line(imgResult, (wT // 2, midY),
+                 (wT // 2 + (curve * 3), midY), (255, 0, 255), 5)
+        cv2.line(imgResult, ((wT // 2 + (curve * 3)), midY - 25),
+                 (wT // 2 + (curve * 3), midY + 25), (0, 255, 0), 5)
         for x in range(-30, 30):
             w = wT // 20
             cv2.line(imgResult, (w * x + int(curve // 50), midY - 10),
@@ -55,17 +61,19 @@ def getLaneCurve(img,display=2):
     elif display == 1:
         cv2.imshow('Resutlt', imgResult)
 
-    #### NORMALIZATION
+    # NORMALIZATION
     curve = curve/100
-    if curve>1: curve ==1
-    if curve<-1:curve == -1
+    if curve > 1:
+        curve == 1
+    if curve < -1:
+        curve == -1
 
     return curve
 
 
 if __name__ == '__main__':
-    cap = cv2.VideoCapture('vid1.mp4')
-    intialTrackBarVals = [102, 80, 20, 214 ]
+    cap = cv2.VideoCapture(0)
+    intialTrackBarVals = [102, 80, 20, 214]
     utlis.initializeTrackbars(intialTrackBarVals)
     frameCounter = 0
     while True:
@@ -75,8 +83,8 @@ if __name__ == '__main__':
             frameCounter = 0
 
         success, img = cap.read()
-        img = cv2.resize(img,(480,240))
-        curve = getLaneCurve(img,display=2)
+        img = cv2.resize(img, (480, 240))
+        curve = getLaneCurve(img, display=2)
         print(curve)
-        #cv2.imshow('Vid',img)
+        # cv2.imshow('Vid',img)
         cv2.waitKey(1)
