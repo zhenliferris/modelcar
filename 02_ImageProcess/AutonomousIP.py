b1996245f3a50fc18b2aae6c2fafcf7fe10a75f3
zhenliferris <liz6@ferris.edu>
2023-04-22 10:01:47 -0400

    add comments

--- 02_ImageProcess/AutonomousIP.py
+++ 02_ImageProcess/AutonomousIP.py
@@ -3,9 +3,10 @@ from LaneModule import getLaneCurve
 import WebcamModule as WebcamModule
 import cv2
 
-###########################################################
+# define motor by BCM pinout number
+# every 3 pins as a set
+# the 2nd and 3rd pins in the set can be switched to adjust the direction of the motor
 motor = Motor(22, 27, 17, 2, 4, 3, 13, 19, 26, 21, 16, 20)
-###########################################################
 
 
 def main():
@@ -14,6 +15,7 @@ def main():
     # for testing, using set 2, for performance using 1
     curveVal = getLaneCurve(img, 1)
 
+    # adjust sensitivity and max speed to achieve better performance
     sen = 0.9  # SENSITIVITY
     maxVAl = 0.1  # MAX SPEED
     if curveVal > maxVAl:
@@ -21,6 +23,7 @@ def main():
     if curveVal < -maxVAl:
         curveVal = -maxVAl
     print(curveVal)
+    # ignore small curves
     if curveVal > 0:
         if curveVal < 0.03:
             curveVal = 0
